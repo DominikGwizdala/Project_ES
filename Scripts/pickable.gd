85157172ff1cdb8352650f1a2d8b33514e2c9bcb
DominikGwizdala <93713131+DominikGwizdala@users.noreply.github.com>
2024-05-16 09:23:32 +0200

    znormalizowane nazwy v1

--- Scripts/pickable.gd
+++ Scripts/pickable.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
 
-
 func _on_body_entered(body): #Sygnał który wykrywa czy nastąpiła kolizja z obiektem Mask to wykrywanie konkretnego Layeru czyli gracz ma Layer 2 a przeciwnik 1 to tylko gracz może to podnieść
 	print("+1")
 	queue_free() #usuwa ze sceny
